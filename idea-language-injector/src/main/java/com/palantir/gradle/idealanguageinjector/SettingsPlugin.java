82cad5d35e4d2625aa7ee77910d7293b9b4e1aad
Finlay Williams <finlayw@palantir.com>
2025-10-14 11:13:42 +0100

    init

---
+++ idea-language-injector/src/main/java/com/palantir/gradle/idealanguageinjector/SettingsPlugin.java
@@ -0,0 +1,11 @@
+package com.palantir.gradle.idealanguageinjector;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.initialization.Settings;
+
+public final class SettingsPlugin implements Plugin<Settings> {
+    @Override
+    public void apply(Settings _settings) {
+
+    }
+}
